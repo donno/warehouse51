d2d15b95954f11811d3087dae5ece0eefc7a6dd5
Sean Donnellan <darkdonno@gmail.com>
2022-06-04 15:07:49 +0930

    [aec] Add scripts for working with Australian Electoral Commission data.
    The preload module is able to read the sumarise the different domain
    names from candidates contact email. However, this information was not
    included in the 2022 federal election. It also summarises profession.

---
+++ aec/mediafeed.py
@@ -0,0 +1,211 @@
+"""Process data from the AEC (Australian Electoral Commission) Media Feed.
+
+Not included:
+- Handling for live elections where data is updated every 2-5 minutes.
+
+
+EML Format
+    See: https://docs.oasis-open.org/election/eml/v5.0/os/EML-Schema-Descriptions-v5.0.html
+
+<EML>
+    TransactionId
+    Count
+        EventIdentifier
+            EventName
+        Election
+            ElectionIdentifier
+            Contests
+                Contest (0 or more)
+</EML>
+
+EventIdentifier
+    Attribute: Id
+        Example: 24310
+EventName
+    Example: 2019 Federal Election
+
+ElectionIdentifier
+    Attribute: Id
+        Example: 24310
+    Element: ElectionName
+        Example: House of Representatives Election
+    Element: ElectionCategory
+        Example: House
+"""
+
+import dataclasses
+import os
+import zipfile
+import pyarrow
+import pyarrow.parquet
+
+try:
+    import defusedxml.ElementTree as ElementTree
+except ImportError:
+    import xml.etree.ElementTree as ElementTree
+
+ELECTIONS_ID_TO_NAME = {
+    '24310', '2019 Federal Election',
+}
+
+ELECTION = 'V:\\24310_2019_Election\\Standard_Eml'
+
+NAMESPACES = {
+    'eml': 'urn:oasis:names:tc:evs:schema:eml',
+}
+
+
+@dataclasses.dataclass
+class Affiliation:
+    identifier: str
+    short_code: str
+    name: str
+
+    @classmethod
+    def from_xml(cls, xml):
+        if xml.tag != f'{{{NAMESPACES["eml"]}}}AffiliationIdentifier':
+            raise ValueError('Wrong XML element, expected '
+                             '"AffiliationIdentifier"')
+
+        name = xml.find('./eml:RegisteredName', NAMESPACES).text
+        return cls(xml.attrib['Id'], xml.attrib['ShortCode'], name)
+
+
+@dataclasses.dataclass
+class Candidate:
+    identifier: str
+    name: str
+    affiliation: Affiliation
+
+    @classmethod
+    def from_xml(cls, xml, affiliation):
+        candidate_id = xml.find('./eml:CandidateIdentifier', NAMESPACES)
+        name = candidate_id.find('./eml:CandidateName', NAMESPACES).text
+        return cls(candidate_id.attrib['Id'], name, affiliation)
+
+
+def eml_contests(eml):
+    """Extracts the contest information from the EML."""
+    needle = './eml:Count/eml:Election/eml:Contests/eml:Contest'
+    for contest in eml.findall(needle, NAMESPACES):
+        yield contest
+
+
+def eml_contests_to_parquet(eml, contests, output_directory, minimised=False):
+    """
+    If minimised is True, then only the IDs for candidates and their
+    affiliation are recorded.
+    """
+
+    # TODO: This only handles a single EML, to handle multiple it needs the
+    # transaction ID added
+
+    if minimised:
+        raise NotADirectoryError('NYI')
+
+    if contests is None:
+        contests = eml_contests(eml)
+
+    # Convert the data to arrays.
+    contest_names = []
+    candidate_names = []
+    affiliation_names = []
+    valid_votes = []
+
+    for contest in contests:
+        contest_id = contest.find('./eml:ContestIdentifier', NAMESPACES)
+        # contest_id.attribs['Id'] and contest_id.attribs['ShortCode']
+        contest_name = contest_id.find('./eml:ContestName', NAMESPACES).text
+
+        for selection in contest.findall('./eml:TotalVotes/eml:Selection',
+                                         NAMESPACES):
+            affiliation_id = selection.find('./eml:AffiliationIdentifier',
+                                            NAMESPACES)
+            if affiliation_id:
+                affiliation = Affiliation.from_xml(affiliation_id)
+            else:
+                affiliation = None
+
+            candidate = Candidate.from_xml(
+                selection.find('./eml:Candidate', NAMESPACES),
+                affiliation)
+
+            votes = int(selection.find('./eml:ValidVotes', NAMESPACES).text)
+
+            # Populate the data.
+            contest_names.append(contest_name)
+            candidate_names.append(candidate.name)
+            affiliation_names.append(affiliation.name if affiliation else '')
+            valid_votes.append(votes)
+
+    arrays = [contest_names, candidate_names, affiliation_names, valid_votes]
+
+    event_id = eml.find('./eml:Count/eml:EventIdentifier', NAMESPACES)
+    event_name = event_id.find('./eml:EventName', NAMESPACES).text
+
+    election_id = eml.find('./eml:Count/eml:Election/eml:ElectionIdentifier',
+                           NAMESPACES)
+    election_name = election_id.find('./eml:ElectionName', NAMESPACES).text
+    election_cat = election_id.find('./eml:ElectionCategory', NAMESPACES).text
+
+    # Generally you each contest together.
+    fields = [
+        ('Contest', pyarrow.string()),
+        ('Candidate', pyarrow.string()),
+        ('Affiliation', pyarrow.string()),
+        ('ValidVotes', pyarrow.int64()),  # Non-negative integer.
+    ]
+
+    # Create a schema
+    schema = pyarrow.schema(
+        fields,
+        metadata={
+            'EventIdentifierName': event_name,
+            'EventIdentifierID': event_id.attrib['Id'],
+            'ElectionIdentifierID': election_id.attrib['Id'],
+            'ElectionName': election_name,
+            'ElectionCategory': election_cat,
+        },
+    )
+
+    # Write it out.
+    writer = pyarrow.parquet.ParquetWriter(
+        os.path.join(output_directory,
+                     event_name.replace(' ', '_') + '.parquet'),
+        schema)
+    writer.write_table(pyarrow.table(arrays, schema=schema))
+    writer.close()
+
+
+def emls(directory):
+    def _files():
+        for file in os.scandir(directory):
+            if all((file.name.startswith('aec-mediafeed-'),
+                    '-Eml-' in file.name,
+                    file.name.endswith('.zip'))):
+                # Zipped
+                yield True, file.path
+            elif file.name.startswith('eml-') and filename.endswith('.xml'):
+                # Uncompressed
+                yield False, file.path
+
+    for compressed, path in _files():
+        if compressed:
+            with zipfile.ZipFile(path) as zipped_eml:
+                xmls = [name for name in zipped_eml.namelist()
+                        if name.startswith('xml/eml') and name.endswith('xml')]
+                for name in xmls:
+                    with zipped_eml.open(name) as eml:
+                        yield ElementTree.parse(eml).getroot()
+
+        else:
+            with open(path) as eml:
+                yield ElementTree.parse(eml).getroot()
+
+
+if __name__ == '__main__':
+    for eml in emls(ELECTION):
+        # print(ElementTree.tostring(eml))
+        eml_contests_to_parquet(eml, None, '')
+        print(eml_contests(eml))
+        break
---
+++ aec/preload.py
@@ -0,0 +1,174 @@
+"""Process pre-load data from the AEC (Australian Electoral Commission).
+
+This is data typically offered at the before the first votes are counted such
+that media companies can load it into their systems.
+
+The intention is they load static election information, for example the
+candidates names before election.
+
+The first preload feed will be available a week after the close of nominations
+and updated in the week before the election.
+"""
+
+import collections
+import zipfile
+
+try:
+    import defusedxml.ElementTree as ElementTree
+except ImportError:
+    import xml.etree.ElementTree as ElementTree
+
+
+NAMESPACES = {
+    'eml': 'urn:oasis:names:tc:evs:schema:eml',
+    'xal': 'urn:oasis:names:tc:ciq:xsdschema:xAL:2.0',
+}
+
+
+class Candidate:
+    def __init__(self, xml_element):
+        self.xml = xml_element
+
+    @property
+    def independent(self) -> bool:
+        assert self.xml.attrib['Independent'] in {'yes', 'no'}
+        return self.xml.attrib['Independent'] == 'yes'
+
+    @property
+    def name(self) -> str:
+        """The name of the candidate from their candidate identifier.
+
+        It is also possible to retrieve more detailed information in the format
+        of OASIS CIQ extensible Name Language (xNL)
+        """
+        return self.xml.find('./eml:CandidateIdentifier/eml:CandidateName',
+                             NAMESPACES).text
+
+    @property
+    def email(self):
+        """The email of the candidate if they provided one."""
+        email = self.xml.find('./eml:Contact/eml:Email', NAMESPACES)
+        return None if email is None else email.text
+
+    @property
+    def profession(self):
+        profession = self.xml.find('./eml:Profession', NAMESPACES)
+        return None if profession is None else profession.text
+
+    def __repr__(self):
+        return f'Candidate("{self.name}")'
+
+
+def load(path, name_fragment):
+    if not zipfile.is_zipfile(path):
+        raise ValueError(f'Expected a ZIP file at the given path ({path})')
+
+    with zipfile.ZipFile(path) as archive:
+        xmls = [name for name in archive.namelist()
+                if name.startswith('xml/') and name.endswith('xml')]
+
+        path = next(zip_path for zip_path in xmls if name_fragment in zip_path)
+        with archive.open(path) as xml:
+            return ElementTree.parse(xml).getroot()
+
+        # ['xml/aec-mediafeed-pollingdistricts-24310.xml',
+        #  'xml/aec-mediafeed-results-detailed-preload-24310.xml',
+        #  'xml/eml-110-event-24310.xml',
+        #  'xml/eml-230-candidates-24310.xml']
+
+
+def load_candidates(path):
+    """Loads the list of candidates for each election for the event.
+
+    For example, the event may be teh 2019 Federal Election, and the two
+    elections taking place are:
+    - House of Representatives Election
+    - Senate Election
+
+    The candidates are broken down by:
+    - Election (Senate or House)
+        - Contest (State or Electoral Division)
+    """
+
+    def _event(candidates):
+        """Return the event information from the candidate list XML element."""
+        event_id = candidates.find('./eml:EventIdentifier', NAMESPACES)
+        return {
+            'id': event_id.attrib['Id'],
+            'name': event_id.find('./eml:EventName', NAMESPACES).text,
+        }
+
+    def _election(election):
+        """Return the election information from an election XML element."""
+        election_id = election.find('./eml:ElectionIdentifier', NAMESPACES)
+        return {
+            'id': election_id.attrib['Id'],
+            'name': election_id.find('./eml:ElectionName', NAMESPACES).text,
+            'category': election_id.find('./eml:ElectionCategory',
+                                         NAMESPACES).text
+        }
+
+    def _contest(contest):
+        """Return the contest information from an contest XML element."""
+        contest_id = contest.find('./eml:ContestIdentifier', NAMESPACES)
+        return {
+            'id': contest_id.attrib['Id'],
+            'short_code': contest_id.attrib.get('ShortCode', None),
+            'name': contest_id.find('./eml:ContestName', NAMESPACES).text,
+        }
+
+    candidates = load(path, '-candidates-')
+    candidates = candidates.find('./eml:CandidateList', NAMESPACES)
+
+    event = _event(candidates)
+
+    elections = candidates.findall('./eml:Election', NAMESPACES)
+
+    for election in elections:
+        # print('------')
+        # print(_election(election))
+
+        contests = election.findall('./eml:Contest', NAMESPACES)
+        for contest in contests:
+            # print(_contest(contest))
+
+            actual_candidates = [
+                Candidate(candidate)
+                for candidate in contest.findall('./eml:Candidate', NAMESPACES)
+            ]
+
+            for c in actual_candidates:
+                yield _election(election), _contest(contest), c
+
+
+PATH = 'data/aec-mediafeed-Detailed-Preload-24310-20190515152735.zip'
+PATH = 'data/aec-mediafeed-Detailed-Preload-27966-20220518111207.zip'
+
+
+if __name__ == '__main__':
+    # for election, contest, candidate in load_candidates(PATH):
+    #     print(election, contest)
+    #     print(candidate)
+
+    def _domain(email: str):
+        return email.partition('@')[2]
+
+    email_domains = {}
+    domain_counter = collections.Counter(
+        _domain(candidate.email)
+        for _, _, candidate in load_candidates(PATH)
+        if candidate.email
+        # and candidate.independent
+    )
+
+    for k, v in domain_counter.most_common(20):
+        print(v, k)
+
+    profession_counter = collections.Counter(
+        candidate.profession
+        for _, _, candidate in load_candidates(PATH)
+        if candidate.profession
+    )
+
+    for k, v in profession_counter.most_common(20):
+        print(v, k)
