e4b95ee3b0fb7e9940232d29f2df2e66b4f57d8a
Sean Donnellan <darkdonno@gmail.com>
2024-01-03 20:15:31 +1030

    [vectortiles] Add simplified style for a tile dataset I'm working on.
    This should make developing the glstype module easier to get started with
    as its not as complicated as the OSM Bright style.
    
    Having said that I only have very minimal interest in continuing with
    the Skia-based renderer from Python, but it would be nice to at least
    get the poygon/line styles for the states working.

--- vectortiles/glstyle.py
+++ vectortiles/glstyle.py
@@ -1,12 +1,15 @@
 """Module for working with Mapbox GL Style JSON.
 
 Specification: https://docs.mapbox.com/mapbox-gl-js/style-spec/
+Newer Specification: https://maplibre.org/maplibre-style-spec/
 
 This is very much incomplete.
 """
 
 import enum
 import json
+import pathlib
+
 
 class LayerType(enum.StrEnum):
     FILL = "fill"
@@ -55,13 +58,23 @@ class Style:
     def __str__(self):
         return f"Style({self['name']}, v{self['version']})"
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str):
         return self.json_document[key]
 
     @property
     def layers(self):
         for layer in self.json_document["layers"]:
             yield StyleLayer(layer)
+
+    def layers_from_source_layer(self, source_layer_name: str):
+        """Find layers given the source layer's name.
+
+        From there the caller can account for the filter.
+        """
+        for layer in self.json_document["layers"]:
+            source_layer = layer.get("source-layer")
+            if source_layer == source_layer_name:
+                yield StyleLayer(layer)
 
 
 class StyleLayer:
@@ -94,8 +107,16 @@ class StyleLayer:
         Required for vector tile sources."""
         return self.json['source_layer']
 
+    def filter_matches(self, context):
+        """Return true if the filter matches this layer given the context."""
+        layer_filter = self.json["filter"]
+        if len(layer_filter) == 1 and layer_filter[0] == "all":
+            return True
+        raise NotImplementedError("Can't handle this type of filter.")
 
-if __name__ == '__main__':
+
+def dev_osm_bright():
+    """Development performed against OSM Bright"""
     with open('osm-bright-gl-style/style.json') as reader:
         style_json = json.load(reader)
 
@@ -104,6 +125,39 @@ if __name__ == '__main__':
     for layer in style.layers:
         if layer.type == LayerType.BACKGROUND:
             print('Background layer')
-        print(layer)
-        break
 
+    print("Land cover layers")
+    layers = style.layers_from_source_layer("landcover")
+    for layer in layers:
+        # TODO: Create way to represent the filters.
+        # print(layer.json["filter"])
+        print(layer.json)
+
+def dev_custom_style():
+    """Development performed against style that I developed."""
+    script_directory = pathlib.Path(__file__).parent
+    with  open(script_directory / 'style-abs.json') as reader:
+        style_json = json.load(reader)
+
+    style = Style(style_json)
+    styles_for_states = style.layers_from_source_layer(
+        "states_and_territories")
+    context = {}
+    for layer in styles_for_states:
+        # Skip it if the filter doesn't apply (this could possibly be merged
+        # with the layers_from_source_layer, once its implemented.
+        if not layer.filter_matches(context):
+            continue
+
+        # From here convert the "paint" to the corresponding paint object.
+        if layer.type == LayerType.FILL:
+            print('Fill layer')
+        elif layer.type == LayerType.LINE:
+            print('Line layer')
+        else:
+            print(f"Unexpected layer type: {layer.type}")
+
+
+if __name__ == '__main__':
+    #dev_osm_bright()
+    dev_custom_style()
