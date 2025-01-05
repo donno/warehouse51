e3905fac6202d500f9f90d54a673f4bb8d73c684
Sean Donnellan <darkdonno@gmail.com>
2025-01-05 21:26:10 +1030

    [chess-viewer] Set-up SDL_ttf and text rendering for the Chess Viewer.

--- chess-viewer-sdl/ui.cpp
+++ chess-viewer-sdl/ui.cpp
@@ -1,4 +1,7 @@
 #include <SDL3/SDL.H>
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include <memory>
 
 constexpr int padding = 80;
 
@@ -45,6 +48,7 @@ void DrawBoard(SDL_Renderer *renderer) {
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
+  TTF_Init();
 
   SDL_Window *window;
   SDL_Renderer *renderer;
@@ -55,8 +59,70 @@ int main() {
     return 3;
   }
 
+  auto ui_font = TTF_OpenFont("IBMPlexSans-Regular.ttf", 16);
+  if (!ui_font) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                 "Failed to load font for user interface: %s", SDL_GetError());
+    return 3;
+  }
+
+  auto pieces_font = TTF_OpenFont("chess_merida_unicode.ttf", 42);
+  if (!pieces_font) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                 "Failed to load font for chess pieces: %s", SDL_GetError());
+    return 3;
+  }
+
+  std::unique_ptr<TTF_TextEngine, decltype(&TTF_DestroyRendererTextEngine)>
+      text_engine(TTF_CreateRendererTextEngine(renderer),
+                  TTF_DestroyRendererTextEngine);
+
+  if (!text_engine) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                 "Failed to load font for chess pieces: %s", SDL_GetError());
+    return 3;
+  }
+
+  SDL_Color text_colour = {255, 255, 255, 255};
+  // The order is King, Queen, Rook, Bishop, Knight, Pawn.
+  // These are the white pieces.
+  const auto outline_pieces_text = "\u2654\u2655\u2656\u2657\u2658\u2659";
+  const auto filled_pieces_text = "\uE254\uE255\uE256\uE257\uE258\uE259";
+  auto pieces =
+      TTF_RenderText_Solid(pieces_font, filled_pieces_text, 0, text_colour);
+  if (!pieces) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                 "Failed to draw chess pieces: %s", SDL_GetError());
+    return 3;
+  }
+
+  auto outlined_pieces =
+      TTF_RenderText_Solid(pieces_font, filled_pieces_text, 0, text_colour);
+  if (!outlined_pieces) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                 "Failed to draw chess pieces: %s", SDL_GetError());
+    return 3;
+  }
+  const SDL_FRect pieces_bounds = {0.0f, 400.0f, pieces->w * 1.0f,
+                                   pieces->h * 1.0f};
+
+  // TODO: Render pieces to a surface and combine.
+  auto pieces_texture = SDL_CreateTextureFromSurface(renderer, pieces);
+  SDL_DestroySurface(pieces);
+
+  std::unique_ptr<TTF_Text, decltype(&TTF_DestroyText)> title(
+      TTF_CreateText(text_engine.get(), ui_font, "Chess Viewer 0.1", 0),
+      TTF_DestroyText);
+
+  // There is a cross hashing behind the pieces but not within the pieces
+  // themselves. const auto outline_cross_hatch_pieces_text =
+  // "\uE154\uE155\uE156\uE157\uE158\uE159";
+  std::unique_ptr<TTF_Text, decltype(&TTF_DestroyText)> pieces_text(
+      TTF_CreateText(text_engine.get(), pieces_font, filled_pieces_text, 0),
+      TTF_DestroyText);
+
   SDL_Surface *surface;
-  for (SDL_Event event; true; ) {
+  for (SDL_Event event; true;) {
     SDL_PollEvent(&event);
     if (event.type == SDL_EVENT_QUIT) {
       break;
@@ -65,10 +131,27 @@ int main() {
     SDL_SetRenderDrawColor(renderer, 38, 36, 33, 0x00);
     SDL_RenderClear(renderer);
     DrawBoard(renderer);
+
+    TTF_DrawRendererText(title.get(), 10, 10);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 0x00);
+
+    // Black pieces
+    TTF_SetTextColor(pieces_text.get(), 0, 0, 0, 255);
+    TTF_DrawRendererText(pieces_text.get(), 10, 500);
+
+    // White pieces
+    TTF_SetTextColor(pieces_text.get(), 255, 255, 255, 255);
+    TTF_DrawRendererText(pieces_text.get(), 10, 600);
+
+    // Render from pieces_texture
+    SDL_RenderTexture(renderer, pieces_texture, nullptr, &pieces_bounds);
+
     SDL_RenderPresent(renderer);
   }
 
+  TTF_CloseFont(ui_font);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
+  TTF_Quit();
   SDL_Quit();
 }
