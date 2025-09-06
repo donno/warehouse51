23ff94fd433611d2d92e075baee732eb0ecf678a
Sean Donnellan <darkdonno@gmail.com>
2025-09-06 21:54:29 +0930

    [snippets] Add modules for creating companies and pairing with users.
    The idea here is to represent users at a set of companies.
    
    This should probably be moved out of snippets now that its more like a
    set of modules.

---
+++ snippets/name_company.py
@@ -0,0 +1,197 @@
+"""Create names of companies.
+
+This simply uses a list of names from another source.
+
+The three sources I considered were:
+* Genus of birds
+* Names of prokaryotics
+* Names of Australia suburbs - this is what I settled on.
+
+TODO
+----
+* Add option to customise the names to add a prefix or suffix such as
+  "Brothers" or "And Co" or "Limited".
+"""
+
+import enum
+import collections
+import csv
+import dataclasses
+import io
+import pathlib
+import random
+import zipfile
+
+
+class UserNameStyle(enum.Enum):
+    """The style of user name's at a given company."""
+
+    FIRST_DOT_LAST = 1
+    """first_name.last_name"""
+
+    F_LAST = 2
+    """First letter of first name and full last name"""
+
+    F_3_DOT_LAST = 3
+    """Up to first three characters for first name and full last name."""
+
+    FOUR_AND_FOUR = 4
+    """Up to first four characters for first name and first four of last name."""
+
+    EMPLOYEE_ID = 5
+    """Assign the employee a number and use that."""
+
+    def __repr__(self):
+        return self.name
+
+
+@dataclasses.dataclass
+class Company:
+    name: str
+    """The name of the company."""
+
+    domain: str
+    """The domain name for the company."""
+    # TODO This may be derived instead for company name, or it might need a
+    # domain name style to decide it.
+
+    user_name_style: UserNameStyle
+    """The style of the usernames at the company.
+
+    This with the domain is used to determine the email address of an employee.
+    """
+
+    head_count: int
+    """The number of people employed by the company."""
+
+    def email_for_user(self, first_name: str, last_name: str, employee_id: int) -> str:
+        """Generate email for a given user.
+
+        The caller will need to check if there are two people with same
+        email and generate unique name for the subsequent people.
+        """
+        match self.user_name_style:  # Historic moment - my first match in Py.
+            case UserNameStyle.FIRST_DOT_LAST:
+                return f"{first_name}.{last_name}@{self.domain}".lower()
+            case UserNameStyle.F_LAST:
+                return f"{first_name[0]}{last_name}@{self.domain}".lower()
+            case UserNameStyle.F_3_DOT_LAST:
+                return f"{first_name[:3]}.{last_name}@{self.domain}".lower()
+            case UserNameStyle.FOUR_AND_FOUR:
+                return f"{first_name[:4]}{last_name:4}@{self.domain}".lower()
+            case UserNameStyle.EMPLOYEE_ID:
+                return f"e{employee_id}@{self.domain.lower()}"
+        raise NotImplementedError
+
+
+def adjust_name(name: str) -> str:
+    """Adjust the name to remove unwanted artifacts.
+
+    For example:
+        "ACT Remainder - " in "ACT Remainder - Cotter River".
+        (State) i.e. " (ACT)"
+    """
+    name = name.partition("(")[0]
+    before, _, after = name.partition(" - ")
+    return (after or before).strip()
+
+
+def state_suburbs():
+    """Return the names and area (KM^2) of suburbs in Australia.
+
+    The data is State Suburbs ASGS Edition 2016 in .csv Format
+    The data is licenced under the Creative Commons Attribution 2.5 Australia.
+    The data source is: https://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.003July%202016?OpenDocument
+    """
+    archive_path = pathlib.Path(__file__).parent / "1270055003_ssc_2016_aust_csv.zip"
+    with zipfile.ZipFile(archive_path) as archive:
+        for info in archive.infolist():
+            path = pathlib.PurePath(info.filename)
+            if path.suffix != ".csv":
+                continue
+
+            with io.TextIOWrapper(
+                archive.open(info, "r"),
+                encoding="utf-8",
+            ) as reader:
+                csv_reader = csv.DictReader(reader)
+                for record in csv_reader:
+                    yield record["SSC_NAME_2016"], float(record["AREA_ALBERS_SQKM"])
+
+
+def merge_state_suburbs():
+    """Return the names and area of suburbs in Australia, without duplicates.
+
+    Duplicates are merged together.
+    """
+    counter = collections.Counter()
+    for name, area in state_suburbs():
+        counter[adjust_name(name)] += int(area * 10**6)  # Square meters.
+    return counter.items()
+
+
+def generate_company_names(n: int) -> list[str]:
+    """Generate the names of a given number of companies (n).
+
+
+    Limitations
+    -----------
+    * This pulls names from an existing list as such n must be smaller than that
+      source list.
+    * Names won't be industry specific due to the source dat.a
+      Maybe one day it will be expanded to do that, such as including particular
+      industries i.e. "Larry's Fruit Pickers". More likely, in the short term
+      it more likely to be extended so you can provide a customise_name function
+      to add to the start or end.
+    """
+
+    # The original idea was to use the area of the suburb to determine the
+    # the size of the company but that was just to wide ranging.
+    choices = [
+        name
+        for name, area in merge_state_suburbs()
+        if area > 0 and " of " not in name
+    ]
+    return random.choices(choices, k=n)
+
+
+def name_to_domain(name: str) -> str:
+    """Convert a company name to a domain.
+
+    This should possibly have option to control the top-level domain  / country
+    and/or use a vanity domain, e.g. .dev, .ai, .shop etc.
+
+    What would be better is country code with list of how many usages there are
+    to use for using as a weighting. Something like
+    https://domainnamestat.com/statistics/tldtype/country but open.
+    https://netapi.com/lists-cctld-domains/
+
+    See https://data.iana.org/TLD/tlds-alpha-by-domain.txt
+    """
+    return f'{name.lower().replace(" ", "")}.example'
+
+
+def generate_companies(n: int) -> list[Company]:
+    """Generate companies of a given number (n)."""
+    names = generate_company_names(n)
+
+    # Make EMPLOYEE_ID a rare option, as its is rather strange.
+    weights = [1 if style is UserNameStyle.EMPLOYEE_ID else 500
+               for style in UserNameStyle]
+    username_styles = random.choices(list(UserNameStyle), weights=weights, k=n)
+
+    return [
+        Company(
+            name=name,
+            # Assume this is international.
+            domain=name_to_domain(name),
+            user_name_style=style,
+            head_count=0,  # Didn't end up making this part of this module.
+        )
+        for name, style in zip(names, username_styles)
+    ]
+
+
+if __name__ == "__main__":
+    for company in generate_companies(10):
+        print(company)
---
+++ snippets/user_generator.py
@@ -0,0 +1,314 @@
+"""Generate company, groups and users.
+
+Create fictional
+- Company / Organisation
+- Groups / Departments / Roles
+- Users / Employees
+
+Components
+----------
+Company
+* Name
+* Domain
+* User name style
+* Head count
+
+Users
+* First name
+* Last Name
+* Email - derived from their company's user name style and domain.
+
+Group
+* Name
+
+Linking
+* Group and Company
+* User and Company
+* User and Group
+
+Output Format
+
+{
+  "companies": [
+     <company>
+  ]
+}
+
+Where company is: (See CompanyJson)
+{
+   "name":
+   "domain":
+   "groups": [],
+   "employees": [
+       <employee>
+   ]
+}
+
+Where employee is: (See EmployeeJson)
+{
+    "first": "John",
+    "last": "Smith",
+    "email": "john.smith@company.example",
+    "groups": ["management"],
+}
+
+To Do
+-----
+* Implement the group assignment.
+"""
+
+import argparse
+import collections
+import itertools
+import json
+import random
+import typing
+
+from name_people import generate_user_names
+from name_company import Company, generate_companies
+
+
+class EmployeeJson(typing.TypedDict):
+    """Represent an employee in JSON as a Python dictionary."""
+
+    first_name: str
+    last_name: str
+    email: str
+    group: list[str]
+
+
+class CompanyJson(typing.TypedDict):
+    """Represent the company in JSON as a Python dictionary."""
+
+    name: str
+    """The name of the company."""
+
+    domain: str
+    """The domain name for the company."""
+
+    user_name_style: str
+    """The name of the style for the user names as used in the emails."""
+
+    employees: list[EmployeeJson]
+    """The employees that work at the company."""
+
+
+def expand(opening_range, repeat_count: int) -> list[int]:
+    """Expand the range by repeating each element by the given repeat count.
+
+    Examples
+    --------
+    >>> expand(range(3), 2)
+    [0, 0, 1, 1, 2, 2]
+    >>> expand(range(3), 3)
+    [0, 0, 0, 1, 1, 1, 2, 2, 2]
+    >>> expand(range(4), 3)
+    [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3]
+    >>> expand('ABC', 2)
+    ['A', 'A', 'B', 'B', 'C', 'C']
+    """
+
+    def _generate():
+        for item in opening_range:
+            yield from itertools.repeat(item, repeat_count)
+
+    return list(_generate())
+
+
+def assign_people_to_company(company_count: int, employee_count: int) -> list[int]:
+    """Assign people as employees to an company.
+
+    The resulting list is a mapping from the employee to the company based
+    on index (i.e. result[0] means employee 0 is at company[0].
+    """
+    small_company_count = int(0.4 * company_count)
+    medium_company_count = int(0.5 * company_count)
+    huge_company_count = int(0.1 * company_count)
+
+    minimum_employees_assigned = (
+        5 * small_company_count + 50 * medium_company_count + 200 * huge_company_count
+    )
+
+    # Take an extra 500 people out to assign to the largest company to give
+    # them an extra edge so there least one company bigger than the rest.
+    reserved_for_largest_company = 500
+    spare = employee_count - minimum_employees_assigned - reserved_for_largest_company
+
+    if spare < 0:
+        raise ValueError(
+            "Either too many companies, too few people or weighting are off."
+        )
+
+    # Some of the companies need more chance then other to get extra employees,
+    # otherwise, they all end up equally getting same number, specially the huge
+    # companies.
+    #
+    # Two ways to try:
+    # 1) Random
+    # 2) Force it to be incremental e. go from 200 to 2000 in increments of 200.
+    #    The problem with this you need enough weights for each company, so you
+    #    need to repeat it or cycle it.
+    company_weights = []
+    company_weights.extend(
+        random.choices(
+            range(-10, 400, 80),
+            k=small_company_count,
+        ),
+    )
+    company_weights.extend(
+        random.choices(
+            range(100, 900, 300),
+            k=medium_company_count,
+        ),
+    )
+    company_weights.extend(
+        random.choices(
+            range(20, 5000, 1000),
+            k=huge_company_count,
+        ),
+    )
+    spare_assignment = random.choices(
+        range(company_count),
+        weights=company_weights,
+        k=spare,
+    )
+
+    # This is where it is a shame there isn't a aversion of range which takes a
+    # start and count. Lets make one.
+    sized_range = lambda start, count: range(start, start + count)
+
+    small_company_assignment = expand(range(small_company_count), 5)
+    medium_company_assignment = expand(
+        sized_range(small_company_count, medium_company_count),
+        50,
+    )
+    huge_company_assignment = expand(
+        sized_range(
+            small_company_count + medium_company_count,
+            huge_company_count,
+        ),
+        200,
+    )
+
+    largest_company = collections.Counter(huge_company_assignment).most_common(1)[0][0]
+    largest_company_extra_assignments = list(
+        itertools.repeat(
+            largest_company,
+            reserved_for_largest_company,
+        ),
+    )
+
+    return (
+        small_company_assignment
+        + medium_company_assignment
+        + huge_company_assignment
+        + spare_assignment
+        + largest_company_extra_assignments
+    )
+
+
+def new_parser() -> argparse.ArgumentParser:
+    """Return an argument parser for tool for generating users."""
+    parser = argparse.ArgumentParser(description="Generate user information.")
+    parser.add_argument(
+        "--users",
+        type=int,
+        default=1000,
+        help="Number of users to generate.",
+    )
+    parser.add_argument(
+        "--companies",
+        type=int,
+        default=10,
+        help="Number of users to generate.",
+    )
+    parser.add_argument(
+        "--json",
+        action="store_true",
+        help="Output the information as JSON.",
+    )
+    return parser
+
+
+def generate_full_description(
+    companies: list[Company],
+    users: list[tuple[str, str]],
+) -> dict[str, list[CompanyJson]]:
+    """Generate a full description of the users, company and groups.
+
+    The resulting dict is JSON-compatible.
+    """
+    person_to_company = assign_people_to_company(arguments.companies, arguments.users)
+
+    company_to_people = {}
+    for person_index, company_index in enumerate(person_to_company):
+        # Since the mapping is not done seperately, rather than use index like
+        # below, sub in the actual user.
+        # company_to_people.setdefault(company_index, []).append(person_index)
+        company_to_people.setdefault(company_index, []).append(users[person_index])
+
+    def convert_person(
+        first_name: str,
+        last_name: str,
+        company: Company,
+    ) -> EmployeeJson:
+        return {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": company.email_for_user(first_name, last_name, 0),
+        }
+
+    def make_email_unique(company: CompanyJson):
+        """Check if emails for employees are unique and make them so if not.
+
+        This is performed in-place.
+        """
+        seen_emails = set()
+        for employee in company["employees"]:
+            email = employee["email"]
+            while email in seen_emails:
+                local_part, _, host = email.partition("@")
+                extra = random.randint(10, 200)
+                email = f"{local_part}{extra}@{host}"
+                employee["email"] = email
+            seen_emails.add(email)
+
+    def convert_company(company: Company, employees: list) -> CompanyJson:
+        company: CompanyJson = {
+            "name": company.name,
+            "domain": company.domain,
+            "user_name_style": company.user_name_style.name,
+            "employees": [
+                convert_person(first_name, last_name, company)
+                for first_name, last_name in employees
+            ],
+        }
+        make_email_unique(company)
+        return company
+
+    return {
+        "companies": [
+            convert_company(company, company_to_people[index])
+            for index, company in enumerate(companies)
+        ],
+    }
+
+
+if __name__ == "__main__":
+    parser = new_parser()
+    arguments = parser.parse_args()
+    user_names = list(generate_user_names(arguments.users))
+    companies = generate_companies(arguments.companies)
+
+    if arguments.json:
+        print(json.dumps(generate_full_description(companies, user_names), indent=2))
+    else:
+        print("Companies")
+        print("=========")
+        for company in companies:
+            print(f"  {company}")
+
+        print("Users")
+        print("=====")
+        for names in user_names:
+            print(*names)
